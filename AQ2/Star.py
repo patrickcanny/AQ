301d12ab955e685ba209a1de03e2e654b1e20ee4
Patrick Canny <patrickcanny@ku.edu>
2017-11-18 00:38:05 -0600

    Restructure

--- AQ2/Star.py
+++
@@ -1,8 +0,0 @@
-class Star(object):
-    def __init__(self, PartialStar = None, OtherStar = None, _bool):
-        super(Star, self).__init__()
-        self.complexes = []
-        self.MyBool = _bool
-        self.PartialStar = PartialStar
-        self.OtherStar = OtherStar
-        
